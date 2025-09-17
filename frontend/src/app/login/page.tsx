1c8fad61a24f546d581bbd0a99109beaa6fcd64d
OmarNajar <omar.mohammad.najjar@gmail.com>
2025-09-17 19:44:05 +0300

    authSlicLogic

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import { useDispatch } from 'react-redux';
+import { setCredentials } from '@/redux/slices/authSlice';
+import { AppDispatch } from '@/redux/store';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function LoginPage() {
+const dispatch = useDispatch<AppDispatch>();
+const router = useRouter();
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+
+const handleLogin = async () => {
+    const res = await fetch('http://localhost:5000/users/login', {
+    method: 'POST',
+    body: JSON.stringify({ email, password }),
+    headers: { 'Content-Type': 'application/json' },
+    });
+
+    const data = await res.json();
+
+    if (data.success) {
+    dispatch(
+        setCredentials({
+        token: data.token,
+        userId: data.user.id,
+        avatarUrl: data.user.avatarUrl,
+        })
+    );
+
+    router.push('/');
+    } else {
+    console.log('Login error');
+    }
+};
+
+return (
+    <div>
+    <div>
+        <h2>Logi</h2>
+        <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+        onClick={handleLogin}>
+        Login
+        </button>
+    </div>
+    </div>
+);
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,18 @@
+'use client';
+
+import { useDispatch,useSelector } from 'react-redux';
+import { useRouter } from 'next/navigation';
+import { AppDispatch } from '@/redux/store';
+
 
 export default function Home() {
+  const dispatch = useDispatch<AppDispatch>();
+  const router = useRouter();
+
+  const token = useSelector((state: any) => state.auth.token);
+console.log(token);
+
+
   return (
     <div>
       <h1>Home</h1>
