e5947103136579ac56d0162980b782ef795147d9
WaleedAlZaben <zabin.waled@gmail.com>
2025-09-22 23:26:11 +0300

    update about page

--- frontend/src/app/test/page.tsx
+++ frontend/src/app/test/page.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import CourseSlider from '../components/CourseSlider/courseSlider'
-import About from '../components/about/page'
 
 function page() {
   return (<>
     <div>
-      <About/>
       <CourseSlider/>
       </div>
     </>
