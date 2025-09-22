20a25269728db8ea2df68e368b3e06ea9c0ca2dc
AseelQabbaha <aseelqabbaha@gmail.com>
2025-09-22 20:54:19 +0300

    modified

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,22 +1,30 @@
+
+"use client"
+
 import { Providers } from './provider';
-import 'bootstrap/dist/css/bootstrap.rtl.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import 'animate.css';
-import './globals.css';
+import ThemeToggle from './components/ThemeToggle/themeToggle';
 import Navbar from './components/Navbar/Navigation';
 import SimpleFooter from './footer/page';
+import UserDialog from './components/userDialog/userDialog';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './globals.css';
+import { useSelector } from 'react-redux';
+import { RootState } from '../redux/store';
+import { useEffect } from 'react';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
         <Providers>
-          <Navbar />
-          <div style={{ marginTop: '4em' }}>
+         
+            <Navbar />
+            <ThemeToggle />
             {children}
-          </div>
-          <SimpleFooter />
+            <UserDialog />
+            <SimpleFooter />
+          
         </Providers>
       </body>
     </html>
--- frontend/src/app/test/page.tsx
+++ frontend/src/app/test/page.tsx
@@ -1,35 +1,26 @@
-import React from 'react'
-import CourseSlider from '../components/CourseSlider/courseSlider'
-<<<<<<< HEAD
+// import React from 'react'
+// import CourseSlider from '../components/CourseSlider/courseSlider'
+// <<<<<<< HEAD
 
-=======
-import About from '../components/about/page'
->>>>>>> 43516806fc0fa7f7dffe0a2273129f54ea82d1a6
-
-
-
-import About from '../components/about/page'
-import UserDialog from "../components/userDialog/userDialog"
-
-function page() {
-  return (<>
-    <div>
-<<<<<<< HEAD
-      
-      <div> 
-        <About/>
-        <CourseSlider/>
-        <UserDialog/>
-       
-        
-=======
-      <About/>
-      <CourseSlider/>
->>>>>>> 43516806fc0fa7f7dffe0a2273129f54ea82d1a6
-      </div>
-    </>
-  )
-}
+// =======
+// import About from '../components/about/page'
+// >>>>>>> 43516806fc0fa7f7dffe0a2273129f54ea82d1a6
 
 
-export default page
+
+// import About from '../components/about/page'
+// import UserDialog from "../components/userDialog/userDialog"
+
+// function page() {
+//   return (<>
+//     <div>
+//       <About/>
+//       <CourseSlider/>
+// <UserDialog/>
+//       </div>
+//     </>
+//   )
+// }
+
+
+// export default page
